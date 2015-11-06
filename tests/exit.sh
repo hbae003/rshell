b7743a9ef2d60b8bea880308ed9696290e459f02
Rabbitmate <peterhsbae@gmail.com>
2015-11-06 01:58:21 -0800

    tests only has exit.sh

---
+++ tests/exit.sh
@@ -0,0 +1,39 @@
+Script started on Fri 06 Nov 2015 01:48:21 AM PST
+]0;hbae003@hammer:~/cs100/hw1/src[?1034h[hbae003@hammer src]$ ls exit
+ls: cannot access exit: No such file or directory
+]0;hbae003@hammer:~/cs100/hw1/src[hbae003@hammer src]$ mkdir exit
+]0;hbae003@hammer:~/cs100/hw1/src[hbae003@hammer src]$ ls[K[Kmkdir exit[3Pls[C[C[C[C[C[3@mkdir[C[C[C[C[C[Kmkdir exit[3Pls[C[C[C[C[C[3Pexitls exit[3@mkdir[C[C[C[C[C[K./a.out
+hbae003@hammer.cs.ucr.edu$ ls exit
+hbae003@hammer.cs.ucr.edu$ ls  ls
+a.out  exit  exit.sh  rshell.cpp
+hbae003@hammer.cs.ucr.edu$ rm -r exit
+hbae003@hammer.cs.ucr.edu$ ls
+a.out  exit.sh	rshell.cpp
+hbae003@hammer.cs.ucr.edu$ ls exit
+ls: cannot access exit: No such file or directory
+hbae003@hammer.cs.ucr.edu$ mkdir exit
+hbae003@hammer.cs.ucr.edu$ ls
+a.out  exit  exit.sh  rshell.cpp
+hbae003@hammer.cs.ucr.edu$ exit
+]0;hbae003@hammer:~/cs100/hw1/src[hbae003@hammer src]$ ./a.out
+hbae003@hammer.cs.ucr.edu$ #exit
+hbae003@hammer.cs.ucr.edu$ ls -a && exit
+.  ..  a.out  exit  exit.sh  rshell.cpp  .rshell.cpp.swp
+]0;hbae003@hammer:~/cs100/hw1/src[hbae003@hammer src]$ ls[K[K./a.outmkdir exit[3P./a.out
+hbae003@hammer.cs.ucr.edu$ exit && exit
+]0;hbae003@hammer:~/cs100/hw1/src[hbae003@hammer src]$ ./a.outmkdir exit[3P./a.out
+hbae003@hammer.cs.ucr.edu$ ex  touch exit
+hbae003@hammer.cs.ucr.edu$ ls
+a.out  exit  exit.sh  rshell.cpp
+hbae003@hammer.cs.ucr.edu$ rm exit
+rm: cannot remove `exit': Is a directory
+hbae003@hammer.cs.ucr.edu$ ls
+a.out  exit  exit.sh  rshell.cpp
+hbae003@hammer.cs.ucr.edu$ rm -r exit
+hbae003@hammer.cs.ucr.edu$ ls
+a.out  exit.sh	rshell.cpp
+hbae003@hammer.cs.ucr.edu$ exit
+]0;hbae003@hammer:~/cs100/hw1/src[hbae003@hammer src]$ exit
+exit
+
+Script done on Fri 06 Nov 2015 01:50:54 AM PST
